26fb774f23ca7dafd52610a00b17a44c0c8bfab0
tipbba <77519251+tipbba@users.noreply.github.com>
2023-11-07 16:59:51 +0800

    Add files via upload

---
+++ thread_gutter.h
@@ -0,0 +1,9 @@
+namespace gutter
+{
+#include "Thread_Gutter/thread_pool/concurrency_deque.h"
+#include "Thread_Gutter/thread_pool/concurrency_queue.h"
+#include "Thread_Gutter/thread_pool/spinlock.h"
+#include "Thread_Gutter/thread_pool/threads_joiner.h"
+#include "Thread_Gutter/thread_pool/thread_pool.h"
+
+} // namespace gutter
